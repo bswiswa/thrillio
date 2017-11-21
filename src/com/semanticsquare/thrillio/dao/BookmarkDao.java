c9ab956aa95881b81a65fead57beeee586c6646e
Batsirai Swiswa <batsiraiswiswa@yahoo.com>
2017-11-20 21:43:07 -0500

    Insert into tables

--- src/com/semanticsquare/thrillio/dao/BookmarkDao.java
+++ src/com/semanticsquare/thrillio/dao/BookmarkDao.java
@@ -1,9 +1,14 @@
 package com.semanticsquare.thrillio.dao;
 
+import com.semanticsquare.thrillio.entities.Book;
 import com.semanticsquare.thrillio.entities.Bookmark;
 import com.semanticsquare.thrillio.entities.UserBookmark;
 import com.semanticsquare.thrillio.entities.WebLink;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -16,10 +21,41 @@ public class BookmarkDao {
 	}
 
 	public void saveUserBookmark(UserBookmark userBookmark) {
-		DataStore.add(userBookmark);
-//		add method adds userBookmark to the userBookmark array
+		try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jid_thrillio?useSSL=false");
+				Statement stmt = conn.createStatement();) {
+			if(userBookmark.getBookmark() instanceof Book) {
+				saveUserBook(userBookmark, stmt);
+			}
+			else if(userBookmark.getBookmark() instanceof WebLink) {
+				saveUserWebLink(userBookmark, stmt);;
+			}
+			else{
+				saveUserMovie(userBookmark, stmt);;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 	
+	private void saveUserMovie(UserBookmark userBookmark, Statement stmt) throws SQLException {
+		String query = "insert into User_Movie (user_id, movie_id) values ("+ 
+				userBookmark.getUser().getId()+", "+ userBookmark.getBookmark().getId() + ")";
+		stmt.executeUpdate(query);
+		
+	}
+
+	private void saveUserWebLink(UserBookmark userBookmark, Statement stmt) throws SQLException {
+		String query = "insert into User_WebLink (user_id, weblink_id) values ("+ 
+				userBookmark.getUser().getId()+", "+ userBookmark.getBookmark().getId() + ")";
+		stmt.executeUpdate(query);	
+	}
+
+	private void saveUserBook(UserBookmark userBookmark, Statement stmt) throws SQLException {
+		String query = "insert into User_Book (user_id, book_id) values ("+ 
+				userBookmark.getUser().getId()+", "+ userBookmark.getBookmark().getId() + ")";
+		stmt.executeUpdate(query);
+	}
+
 	public List<WebLink> getAllWebLinks(){
 		List<WebLink> result = new ArrayList<>();
 		Map<Integer, List<Bookmark>> bookmarks = getBookmarks();
